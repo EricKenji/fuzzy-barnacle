1194a460f1f9faaac945ddd29df396c395d95871
Eric Furukawa <eric.furukawa@gmail.com>
2022-02-27 08:52:08 -0800

    added profile pic and description to aboutme

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PortPic from '../../assets/images/portpic.jpg'
 
 function AboutMe() {
 
@@ -10,10 +11,14 @@ function AboutMe() {
                 </h1>
             </div>
             <div>
-                PICTURE GOES HERE
+                <img src={PortPic} alt='profile picture' />
             </div>
             <div>
-                <p>My name is Eric and this is my about me section</p>
+                <p>Welcome to my Portfolio! My name is Eric Furukawa.  I am an aspiring full-stack developer based out of Sacramento, CA. </p>
+                <p>In 2021, due to the changing job landscape and my love for learning, I made the jump into becoming a web developer. </p>
+                <p>I decided to enroll in a 6 month coding bootcamp to jump start my foray into my new career path.</p>
+                <p>Up to this point, I have focused on Javascript, more specifically, MERN stack</p>
+                <p>For fun, my hobbies include traveling, watching sports, and playing video games.</p>
             </div>
         </section>
     );
