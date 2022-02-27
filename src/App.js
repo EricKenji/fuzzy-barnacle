e1ee59d130f1b2a7e7e097a6d61ce33d6b986913
Eric Furukawa <eric.furukawa@gmail.com>
2022-02-27 07:27:33 -0800

    created About Me section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
 
 function App() {
 
@@ -7,7 +8,7 @@ function App() {
     <div>
       <Header></Header>
       <main>
-
+        <AboutMe></AboutMe>
       </main>
     </div>
   )
---
+++ src/components/AboutMe/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function AboutMe() {
+
+    return (
+        <section>
+            <div>
+                <h1>
+                    ABOUT ME
+                </h1>
+            </div>
+            <div>
+                PICTURE GOES HERE
+            </div>
+            <div>
+                <p>My name is Eric and this is my about me section</p>
+            </div>
+        </section>
+    );
+}
+
+export default AboutMe;
