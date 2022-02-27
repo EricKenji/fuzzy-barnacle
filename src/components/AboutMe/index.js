adcf19495cac453b2c1406c52849a2829dd3194a
Eric Furukawa <eric.furukawa@gmail.com>
2022-02-27 09:00:05 -0800

    added styling to Nav

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -16,7 +16,7 @@ function AboutMe() {
             <div>
                 <p>Welcome to my Portfolio! My name is Eric Furukawa.  I am an aspiring full-stack developer based out of Sacramento, CA. </p>
                 <p>In 2021, due to the changing job landscape and my love for learning, I made the jump into becoming a web developer. </p>
-                <p>I decided to enroll in a 6 month coding bootcamp to jump start my foray into my new career path.</p>
+                <p>I decided to enroll in a 6 month coding bootcamp to jump start my foray into a new career path.</p>
                 <p>Up to this point, I have focused on Javascript, more specifically, MERN stack</p>
                 <p>For fun, my hobbies include traveling, watching sports, and playing video games.</p>
             </div>
